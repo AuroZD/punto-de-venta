389b88a3e086986d1c4d3b87a13b03c8f583fdd5
AuroZD <aurelianoac.zdico@gmail.com>
2023-12-11 16:59:42 -0600

    pseudocodigo y prueba de switch

--- PuntoDeVenta/PuntoDeVenta/PuntoDeVenta.cpp
+++ PuntoDeVenta/PuntoDeVenta/PuntoDeVenta.cpp
@@ -1,20 +1,73 @@
-// PuntoDeVenta.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* pseudo codigo
+inicio del programa
+	declaracion de variables (productos, precios, anticipo(V,F) ) (estructura)
+	casos (que producto)
+	n = cantidad de productos
+	para caso n
+		determinar cantidad
+		calcular coste acorde a cantidad
+			inicio si	
+				si requiere anticipo
+					indicar cuanto fue anticipado
+				si no
+					preguntar si va a anticipar
+					inicio si
+						si anticipa
+							guardar anticipo
+						si no
+					fin del si
+				fin del si 
+			abrir archivo
+				añadir a archivo
+					producto
+					cantidad
+					costo
+					anticipo
+				fin de añadir
+			cerrar archivo
 
-#include <iostream>
+fin del programa
+*/
 
-int main()
+#include <stdio.h>
+
+struct producto
 {
-    std::cout << "Hello World!\n";
+	float precio;
+	float anticipo;
+};
+//funcion para que sea acorde al producto
+
+void calculador()
+{
 }
+void main()
+{
+	//lista de productos
+	producto taza;
+	taza.precio = 90;
+	taza.anticipo = 0;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	//variables
+	int eleccion;
+	//interfaz con texto que indique los productos existentes
+	printf("Lista de productos:\n");
+	printf("\tTaza\t\t1\n");
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+	//determinar a que producto se le va a  hacer la operacion
+	printf("Que producto vas a vender?(numero del producto):");
+
+	scanf_s("%i", &eleccion);
+
+	switch (eleccion)
+	{
+	case 1:
+			printf("\nElegiste opcion 1\n");
+			//dummie
+			printf("la taza cuesta %.2f y requiere anticipo de %.2f\n", taza.precio, taza.anticipo);
+			break;
+	default:
+			break;
+	}
+}
